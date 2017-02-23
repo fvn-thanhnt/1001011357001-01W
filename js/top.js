a9f3c2e484e7c65326453997362613c206a6943a
fvn-stanfoot <fvn.knowledge@gmail.com>
2017-02-23 11:25:28 +0700

    ThanhNT Index Update 170223

--- js/top.js
+++ js/top.js
@@ -1,2 +1,26 @@
-
+$(document).ready(function(e) {
+    $("#index_slider").slick({
+		dots: false,
+	  slidesToShow: 3,
+	  centerMode: true,
+	  variableWidth: true,
+	  infinite: true,
+	  autoplay: true,
+      autoplaySpeed: 3000,
+	  slidesToScroll: 1,
+	  arrows: false,
+	});
 	
+	$("#index_box03_list").slick({
+		dots: false,
+	  slidesToShow: 5,
+	  centerMode: true,
+	  centerPadding: '50px',
+	  variableWidth: true,
+	  infinite: true,
+	  autoplay: true,
+      autoplaySpeed: 3000,
+	  slidesToScroll: 1,
+	  arrows: false,
+	});
+});
